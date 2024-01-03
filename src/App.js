a6e6dc0cc00164cd260885f65026ed9f422527a7
Prince Otchere <otchereprince00@gmail.com>
2024-01-03 11:09:06 -0500

    lorem ipsum and react logo spin added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar";
 import "./index.css";
@@ -25,8 +25,10 @@ function App() {
       <Navbar />
       <div className="container">
         <article>
-          <h1>What is Lorem Ipsum? </h1>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+          <h1>React app</h1>
+          Coming soon...
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <img src={logo} className="App-logo" alt="logo" />
         </article>
       </div>
     </div>
