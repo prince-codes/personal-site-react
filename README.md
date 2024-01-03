d8c5a61c837ac5e81395e3fbd0cbcae794cf1748
Prince Otchere <otchereprince00@gmail.com>
2024-01-03 08:43:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# personal-site-react
+ New site for 2024
---
+++ src/constants/data.js
@@ -0,0 +1,7 @@
+export const data = {
+    firstName: "Prince",
+    lastName: "Otchere",
+    instagram: "https://instagram.com/princecodes.io",
+    twitter: "https://x.com/princecodesio",
+    github: "https://github.com/prince-codes",
+}
